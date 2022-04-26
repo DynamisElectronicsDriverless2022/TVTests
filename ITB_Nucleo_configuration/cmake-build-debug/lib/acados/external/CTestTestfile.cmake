242f822c873d2f8185a2b93c7f9f7afe121d47e7
MarioBertelli <mario.bertelli@mail.polimi.it>
2022-04-26 12:50:05 +0200

    Removed remote gitignored files

--- ITB_Nucleo_configuration/cmake-build-debug/lib/acados/external/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/DynamisGit/Timing/TVTests/ITB_Nucleo_configuration/acados/external
-# Build directory: D:/DynamisGit/Timing/TVTests/ITB_Nucleo_configuration/cmake-build-debug/lib/acados/external
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("blasfeo")
-subdirs("hpipm")
